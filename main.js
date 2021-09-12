e6f5eb096444bf4a16b593b3972da46182ca8085
Ryanvijogeorge <71065624+Ryanvijogeorge@users.noreply.github.com>
2021-09-12 17:15:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+var tals = ["Video Editing", "Playing Keyboard", "Coding", "Playing Football", "Academics", "Guitar", "Karate", "Being the Best Friend Ever"];
+
+function shotals(){
+    document.getElementById("display_tals").style.visibility = true;
+}
